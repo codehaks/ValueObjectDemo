d515f390d3eef3e88c4e1ec73264f3f4200bc390
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-22 01:37:20 +0330

    Money

---
+++ MyApp/Models/Money.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Portal.Domain.Values
+{
+    public class Money
+    {
+        public Money(int value)
+        {
+            if (value >= 0)
+            {
+                Value = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Can not be less than 0");
+            }
+        }
+        public int Value { get; private set; }
+
+    }
+}
